159d20895b240ccbc39555c0231c8c8f1a6e156e
yolandadastile <32202347-yolandadastile@users.noreply.replit.com>
2024-02-20 06:42:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# from flask import Flask
+
+# app = Flask('app')
+
+# @app.route('/')
+# def hello_world():
+#   return 'Hello, World!'
+
+# app.run(host='0.0.0.0', port=8080)
+
+# temperature = float(input("Please enter a temperature in ferenheit "))
+# convert = (temperature - 32) * (5 / 9)
+# print(f"Temperature in celsius {convert} C")
+
+# birth_year = input("Enter the year you were born in ")
+# age = 2024 - int(birth_year)
+# print(f"You are {age} years old")
+# import math
+# r = input("Enter radius ")
+# area = math.pi*float(r)**2
+# print(f"The area of the circle is {area}")
+
+# num = input("Enter a number ")
+# eq = int(num) // 10
+# spaces = 10 - eq
+# print(f'[{"=" * eq} {" " * spaces}] {num}%')
