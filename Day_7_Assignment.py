231f54df325a7eab93fa0dff255e462b35fc5034
Yollandaa <yolanda.dastile@gmail.com>
2024-02-28 14:57:43 +0200

    Formatting changes using black formatter

--- Day_7_Assignment.py
+++ Day_7_Assignment.py
@@ -6,6 +6,8 @@
 # 3.  3  3 Mar    deposit        7000
 from datetime import datetime
 
+
+# PEP 8: Python Enhancement Proposals
 class Bank:
     def __init__(self, acc_no, name, balance):
         self.acc_no = acc_no
@@ -20,28 +22,30 @@ class Bank:
         if amount > self.balance:
             return "Failure. Insufficient balance"
         self.balance -= amount
-        self.add_transaction(len(self.transactions) + 1, datetime.now(), 'withdraw', amount)
+        self.add_transaction(
+            len(self.transactions) + 1, datetime.now(), "withdraw", amount
+        )
         return f"Success. Your balance is: R{self.balance:,}"
 
     def deposit(self, amount):
         self.balance += amount
-        self.add_transaction(len(self.transactions) + 1, datetime.now(), 'deposit', amount)
+        self.add_transaction(
+            len(self.transactions) + 1, datetime.now(), "deposit", amount
+        )
         return f"Success. Your balance is: R{self.balance:,}"
-    
+
     def add_transaction(self, id, date, type, amount):
-        transaction = {
-            'id': id,
-            'date': date,
-            'type': type,
-            'amount': amount
-        }
+        transaction = {"id": id, "date": date, "type": type, "amount": amount}
         self.transactions.append(transaction)
-    
+
     def print_statement(self):
         print(f"id\tDate  \t  Type\t\tAmount")
         for transaction in self.transactions:
-            print(f"{transaction['id']:<5} {transaction['date'].strftime('%d %b'):<10} {transaction['type']:<10} {transaction['amount']:>10}")
-    
+            print(
+                f"{transaction['id']:<5} {transaction['date'].strftime('%d %b'):<10} {transaction['type']:<10} {transaction['amount']:>10}"
+            )
+
+
 gemma = Bank("123", "Gemma Porrill", 15_000)
 dhara = Bank("124", "Dhara Kara", 50_001)
 caleb = Bank("125", "Caleb Potts", 100_000)
