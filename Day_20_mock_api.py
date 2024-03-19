860014b95dd64f34e622ca8243e14c6f49b5badf
Yollandaa <yolanda.dastile@gmail.com>
2024-03-19 14:36:42 +0200

    Day 21: More apis

--- Day_20_mock_api.py
+++ Day_20_mock_api.py
@@ -51,6 +51,42 @@ async def delete_user(user_id):
 # asyncio.run(main())
 
 
+# Task 1: Create a new user
+async def create_user(new_user):
+    url = "https://65f8285ab4f842e80887137c.mockapi.io/users"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=new_user) as response:
+            users = await response.json()
+            return users
+
+
+# async def main():
+#     # get all the users: for each user change avatar key to
+#     new_user = {
+#         "name": "Caleb Potts",
+#         "avatar": "https://media.licdn.com/dms/image/C4D03AQHxbiuJcNSm3w/profile-displayphoto-shrink_200_200/0/1566069760288?e=2147483647&v=beta&t=GsaCn0lHpyZZhJErdLBKSRpkUJfiNPXssyzbGc8XckI",
+#     }
+
+#     user = await create_user(new_user)
+#     print(user)
+
+# asyncio.run(main())
+
+
+# Task 2: Create 5 users with the same avatar
+# async def main():
+#     user_list = ["Alex", "Gemma", "Thato", "Lilitha", "Dhara"]
+#     avatar_url = (
+#         "https://th.bing.com/th/id/OIP.K4XfRyYNnlwsR9-qdE-lrQAAAA?rs=1&pid=ImgDetMain"
+#     )
+#     users_data = [create_user({"name": user, "avatar": avatar_url}) for user in user_list]
+#     users_gather = await asyncio.gather(*users_data)
+#     print(users_gather)
+
+
+# asyncio.run(main())
+
+
 # Task 2: Change all users' avaters to human rights flag
 async def change_avatar(user_id, data):
     url = f"https://65f8285ab4f842e80887137c.mockapi.io/users/{user_id}"
@@ -63,12 +99,13 @@ async def change_avatar(user_id, data):
 async def main():
     # get all the users: for each user change avatar key to
     users = await get_users()
-    avatar_url = "https://www.shutterstock.com/image-vector/human-rights-day-south-africa-600w-1937647653.jpg"
+    avatar_url = (
+        "https://th.bing.com/th/id/OIP.K4XfRyYNnlwsR9-qdE-lrQAAAA?rs=1&pid=ImgDetMain"
+    )
     data = {"avatar": avatar_url}
 
     ids_co_routines = [change_avatar(user["id"], data) for user in users]
     id_data = await asyncio.gather(*ids_co_routines)
-
     print(id_data)
 
 
--- Day_20_more_sync.py
+++ Day_20_more_sync.py
@@ -1,107 +1,121 @@
 import asyncio
 
 
-# async def background_task():
-#     print("Start Background Task")
+# # async def background_task():
+# #     print("Start Background Task")
+# #     await asyncio.sleep(3)
+
+# # Async no event loop
+# # async def main():
+# #     await background_task()
+# #     print("Main function says - Hi")
+
+
+# # asyncio.run(main())
+# # We don't want the await background task
+
+
+# # This one first prints all the mains and prints the background_task after the main are done
+# # This happens concurrently
+# async def cooking_eggs():
+#     print("Eggs cooking")
 #     await asyncio.sleep(3)
-
-# Async no event loop
-# async def main():
-#     await background_task()
-#     print("Main function says - Hi")
-
-
-# asyncio.run(main())
-# We don't want the await background task
+#     print(
+#         "Eggs cooked"
+#     )  # does not see this print statement because the main task is done
+#     return f"Data - Eggs 🥚"
 
 
-# This one first prints all the mains and prints the background_task after the main are done
-# This happens concurrently
-async def cooking_eggs():
-    print("Eggs cooking")
-    await asyncio.sleep(3)
-    print(
-        "Eggs cooked"
-    )  # does not see this print statement because the main task is done
-    return f"Data - Eggs 🥚"
+# async def make_coffee():
+#     print("Coffee brewing")
+#     await asyncio.sleep(2)
+#     print("Coffe done")
+#     return f"Data - Coffee ☕"
 
 
-async def make_coffee():
-    print("Coffee brewing")
-    await asyncio.sleep(2)
-    print("Coffe done")
-    return f"Data - Coffee ☕"
+# async def make_cereal():
+#     print("Making Cereal bowl")
+#     await asyncio.sleep(5)
+#     print("Cereal done")
+#     return f"Data - Cereal 🥣"
 
 
-async def make_cereal():
-    print("Making Cereal bowl")
-    await asyncio.sleep(5)
-    print("Cereal done")
-    return f"Data - Cereal 🥣"
-
-
-async def main():
-    # Request to event loop to schedule
-    task1 = asyncio.create_task(cooking_eggs())  # happens concurrently
-    task2 = asyncio.create_task(make_coffee())  # happens concurrently
-    # Waiting for the background task
-    print("Main function says - Hi")
-    print("Main function says - Hi")
-    print("Main function says - Hi")
-
-    # await task1  # This waits for the eggs_cooking task to finish
-
-    # What if we do not know the longest task is: Wait for all tasks to finish (a set of tasks)
-    await asyncio.wait({task1, task2})
-
-
-# asyncio.run(main())
-
-
-# # Using gather
 # async def main():
 #     # Request to event loop to schedule
-# The schedule is happening
-#     all_tasks = [
-#         asyncio.create_task(cooking_eggs()),
-#         asyncio.create_task(make_coffee()),
-#         asyncio.create_task(make_cereal()),
+#     task1 = asyncio.create_task(cooking_eggs())  # happens concurrently
+#     task2 = asyncio.create_task(make_coffee())  # happens concurrently
+#     # Waiting for the background task
+#     print("Main function says - Hi")
+#     print("Main function says - Hi")
+#     print("Main function says - Hi")
+
+#     # await task1  # This waits for the eggs_cooking task to finish
+
+#     # What if we do not know the longest task is: Wait for all tasks to finish (a set of tasks)
+#     await asyncio.wait({task1, task2})
+
+
+# # asyncio.run(main())
+
+
+# # # Using gather
+# # async def main():
+# #     # Request to event loop to schedule
+# # The schedule is happening
+# #     all_tasks = [
+# #         asyncio.create_task(cooking_eggs()),
+# #         asyncio.create_task(make_coffee()),
+# #         asyncio.create_task(make_cereal()),
+# #     ]
+# #     # Waiting for the background task
+# #     print("Main function says - Hi 1")
+# #     print("Main function says - Hi 2")
+# #     print("Main function says - Hi 3")
+
+# #     # await task1  # This waits for the eggs_cooking task to finish
+# #     data = await asyncio.gather(*all_tasks)  # Unpacking the tasks
+# #     print(data)
+
+
+# # asyncio.run(main())
+
+# # Another way to do it
+
+
+# # All async functions return a coroutine object
+# # We use gather cuz you don't need to say create_task
+# async def main():
+#     # No scheduling is happening here
+#     all_co_routines = [
+#         cooking_eggs(),
+#         make_coffee(),
+#         make_cereal(),
 #     ]
+
 #     # Waiting for the background task
 #     print("Main function says - Hi 1")
 #     print("Main function says - Hi 2")
 #     print("Main function says - Hi 3")
 
-#     # await task1  # This waits for the eggs_cooking task to finish
-#     data = await asyncio.gather(*all_tasks)  # Unpacking the tasks
+#     await asyncio.sleep(10)
+
+#     # Scheduling is only triggered here with the gather
+#     data = await asyncio.gather(*all_co_routines)  # Unpacking the tasks
 #     print(data)
 
 
 # asyncio.run(main())
 
-# Another way to do it
+
+async def timer(name, delay):
+    await asyncio.sleep(delay)
+    print(f"{name} is done after {delay}")
+    return f"Data - {name}"
 
 
-# All async functions return a coroutine object
-# We use gather cuz you don't need to say create_task
 async def main():
-    # No scheduling is happening here
-    all_co_routines = [
-        cooking_eggs(),
-        make_coffee(),
-        make_cereal(),
-    ]
-
-    # Waiting for the background task
-    print("Main function says - Hi 1")
-    print("Main function says - Hi 2")
-    print("Main function says - Hi 3")
-
-    await asyncio.sleep(10)
-
-    # Scheduling is only triggered here with the gather
-    data = await asyncio.gather(*all_co_routines)  # Unpacking the tasks
-    print(data)
+    output = await asyncio.gather(timer("slow task", 3), timer("fast task", 1))
+    print(output)
 
 
 asyncio.run(main())
